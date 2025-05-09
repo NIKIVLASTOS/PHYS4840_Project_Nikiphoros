d8a2c0b95e749b70e05965e45bdb4101086f264f
NIKIVLASTOS <159208334+NIKIVLASTOS@users.noreply.github.com>
2025-05-09 09:26:19 -0600

    Update normalization.f90

--- normalization.f90
+++ normalization.f90
@@ -1,22 +1,22 @@
-!==============================                                              
+!=================                                              
 ! File: normalization.f90                                                   
-!==============================
+!===================
 
-module normalization                                                         ! Define module "normalization routines"
-  implicit none                                                              ! Require all variables to be explicitly declared (Nothing is implicit)
+module normalization                                                         ! module "normalization routines"
+  implicit none                                                              ! require all variables to be explicitly declared (Nothing is implicit)
 contains                                                                     ! start
 
   subroutine normalize_wavefunctions(psi, dx)                               ! Subroutine to normalize each wavefunction psi(x)
     implicit none                                                            ! no implicit
-    real(8), intent(in) :: dx                                                ! Input: grid spacing
-    real(8), intent(inout) :: psi(:,:)                                       ! Input/output: matrix of wavefunctions (nstates * Nx)
+    real(8), intent(in) :: dx                                                ! Input of grid spacing
+    real(8), intent(inout) :: psi(:,:)                                       ! Input/output of matrix of wavefunctions (nstates * Nx)
     integer :: n                                                             ! Index for wavefunction
-    real(8) :: norm                                                          ! Norm of each wavefunction
+    real(8) :: norm                                                          ! norm of each wavefunction
 
     do n = 1, size(psi, 1)                                                   ! Loop over each wavefunction
       norm = sqrt(sum(psi(n,:)**2) * dx)                                     ! Compute L2 norm using rectangle rule: integral of psi^2 dx approx equals sum * dx
-      psi(n,:) = psi(n,:) / norm                                             ! Normalize: by doing psi / absolute of psi 
+      psi(n,:) = psi(n,:) / norm                                             ! Normalize by doing psi / absolute of psi 
     end do                                                                   ! End loop
   end subroutine normalize_wavefunctions                                     ! End subroutine
 
-end module normalization                                                     ! End module
+end module normalization                                                     ! End whole program
